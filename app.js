35f0bbe39cb54e36349e1fa5a4f092f288f85e45
bartosian <bartos2013@mail.ru>
2018-04-30 13:04:29 -0700

    Debug

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'app_public', 'build')));
 app.use('/api', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://relax-yourself.herokuapp.com');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type,Accept');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
-
 app.use('/api', api);
 app.get(/(\/about)|(\/location\/[a-z0-9]{24})/, function(req, res, next) {
     res.sendFile(path.join(__dirname, 'app_public', 'build', 'index.html'));
